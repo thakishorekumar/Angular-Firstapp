519d97889156c6ea5f3b42770d0dbe0e9bc1d34a
thakishorekumar <tha.kishorekumar@gmail.com>
2021-01-01 13:51:30 +0530

    first-app
    first-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,61 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { OneComponent } from './one/one.component';
+import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { StaffsComponent } from './staffs/staffs.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
+
+const routes: Routes = [
+  {path:'one',component:OneComponent},
+  {path:'students',component:StudentsComponent},
+  {path:'staffs',component:StaffsComponent},
+  {path:'attendance',component:AttendanceComponent},
+  {path:'feedback',component:FeedbackComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    StudentsComponent,
+    StaffsComponent,
+    AttendanceComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule.forRoot(routes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatProgressSpinnerModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
